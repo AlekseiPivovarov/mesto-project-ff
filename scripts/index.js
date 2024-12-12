76b0f23ba70613df31409a16f2fb6fd97bbb9d7f
Aleksei Pivovarov <aleksei-pivovarov_95@mail.ru>
2024-12-12 19:07:18 +0300

    Ссозданы функция создания, кнопки удаления, создание карточек

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,28 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+
+function createDeleteButton(element) {
+    let deleteButton = element.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', function () {
+        let cardItem = deleteButton.closest('.card');
+        cardItem.remove();
+    })
+}
+
+function createCard(name, link) {
+    const cardTemplate = document.querySelector('#card-template').content; 
+    const placesList = document.querySelector('.places__list');
+
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    cardElement.querySelector('.card__image').src = link;
+    cardElement.querySelector('.card__title').textContent = name;
+    placesList.append(cardElement);
+    createDeleteButton(cardElement);
+}
+
+
+initialCards.forEach((item)=>{
+    createCard(item.name, item.link);
+})
